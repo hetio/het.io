b77a739a15f395d7af892c872925330a0899deee
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-09-09 15:27:51 -0400

    Add description tooltips for compounds and diseases

--- repurpose/js/utilities.js
+++ repurpose/js/utilities.js
@@ -1,4 +1,4 @@
-var data_url = 'https://raw.githubusercontent.com/dhimmel/het.io-rep-data/1dfe1ea82a51e2a5a2626645303a5b03c63dd376/browser-tables/'
+var data_url = 'https://raw.githubusercontent.com/dhimmel/het.io-rep-data/1a960f0e353586f8fe9f61b569919f24603d4344/browser-tables/';
 
 function getUrlVars() {
   var vars = {};
@@ -23,4 +23,17 @@ var categories = {DM: 0, SYM: 1, NOT: 3};
 function render_category(data, type) {
   if (type != 'sort') {return data};
   return data == null ? 4 : categories[data];
+}
+
+function render_name(data, type, row) {
+  if (type != 'display') {return data};
+  var description = row[row.length - 1];
+  if (!description) {return data};
+  return `<span data-toggle="tooltip" data-placement="auto bottom" title="${description}">${data}</span>`;
+}
+
+function activate_tooltip() {
+  $(document).ready(function(){
+    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+  });
 }
