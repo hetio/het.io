726f5e0f93a3cbd2bc8060d50047b16315b71186
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-06-21 11:32:01 -0400

    delete vestigial files, reindent

--- scripts.js
+++ scripts.js
@@ -1,46 +1,46 @@
 // create section links next to each heading of certain type
 function createAnchors() {
-    const headers = document.querySelectorAll('h3');
-    for (const header of headers) {
-        // find closest parent element with id
-        let element = header;
-        while (!element.id && element !== document.body)
-            element = element.parentElement;
-        const id = element.id;
-        if (!id)
-            continue;
+  const headers = document.querySelectorAll('h3');
+  for (const header of headers) {
+    // find closest parent element with id
+    let element = header;
+    while (!element.id && element !== document.body)
+      element = element.parentElement;
+    const id = element.id;
+    if (!id)
+      continue;
 
-        // create link object
-        const link = document.createElement('a');
-        link.classList.add('anchor');
-        link.innerHTML = '<i class="fas fa-link fa-sm"></i>';
-        link.href = '#' + id;
-        header.appendChild(link);
-    }
+    // create link object
+    const link = document.createElement('a');
+    link.classList.add('anchor');
+    link.innerHTML = '<i class="fas fa-link fa-sm"></i>';
+    link.href = '#' + id;
+    header.appendChild(link);
+  }
 }
 
 // glow section when user navigates to it
 function onHashChange() {
-    const id = window.location.hash.replace('#', '');
-    const element = document.getElementById(id);
-    if (!element)
-        return;
+  const id = window.location.hash.replace('#', '');
+  const element = document.getElementById(id);
+  if (!element)
+    return;
 
-    // start css glow animation
-    element.setAttribute('data-glow', 'true');
-    window.setTimeout(() => element.removeAttribute('data-glow'), 2000);
+  // start css glow animation
+  element.setAttribute('data-glow', 'true');
+  window.setTimeout(() => element.removeAttribute('data-glow'), 2000);
 }
 
 // prevent font awesome icons from wrapping to new line alone
 function wrapIcons() {
-    const icons = document.querySelectorAll('i.fas, i.fab');
-    for (const icon of icons) {
-        const parent = icon.parentNode;
-        const wrapper = document.createElement('span');
-        parent.replaceChild(wrapper, icon);
-        wrapper.classList.add('icon');
-        wrapper.appendChild(icon);
-    }
+  const icons = document.querySelectorAll('i.fas, i.fab');
+  for (const icon of icons) {
+    const parent = icon.parentNode;
+    const wrapper = document.createElement('span');
+    parent.replaceChild(wrapper, icon);
+    wrapper.classList.add('icon');
+    wrapper.appendChild(icon);
+  }
 }
 
 window.addEventListener('load', createAnchors);
